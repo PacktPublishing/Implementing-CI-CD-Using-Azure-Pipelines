049f4f2761422c439f0cfe274ae65956b0b128e0
Roberto Mardeni <rmardeni@hotmail.com>
2023-04-15 16:35:27 -0400

    Added SonarQube

---
+++ SonarQubeIntegration/Environments/CustomScripts/InstallWebServerModules.ps1
@@ -0,0 +1,17 @@
+#use tsl 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Install Nuget, needed to install DSC modules via PowerShellGet
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
+# Install required PowerShell Modules
+Install-Module -Name cChoco -Force
+
+function Disable-InternetExplorerESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
+}
+
+Disable-InternetExplorerESC
---
+++ SonarQubeIntegration/README.md
@@ -0,0 +1,10 @@
+# Overview
+A sample repository of a ficticious online ecommerce store, used to demonstrate the SAST and SCA capabilities of SonarQube analysis.
+
+## Getting Started
+1.	Clone the repo
+2.	Inspect the code
+3.	Import the azure-pipelines-with-sonarqube.yaml in Azure Pipelines
+
+## Build and Test
+Execute the pipeline and you will be able to see the execution of SonarQube analysis and link to results.
